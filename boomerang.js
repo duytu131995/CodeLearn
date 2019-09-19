3f9bc4ddce43e6eeab94bab82de64de683d0a2fe
duytu131995 <55371207+duytu131995@users.noreply.github.com>
2019-09-19 10:09:26 +0700

    Create boomerang.js

---
+++ boomerang.js
@@ -0,0 +1,10 @@
+//chua nop
+function boomerang(arr){
+    sum = 0;
+    for( var i = 0; i < arr.length; i++ ){
+        if( arr[i] !== arr[i+1] && arr[i] === arr[i+2] ){
+            sum++;
+        }
+    }
+    return sum;
+}
