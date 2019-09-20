dc274130a6a03cf28a17f750004a48fedbc49b68
duytu131995 <55371207+duytu131995@users.noreply.github.com>
2019-09-20 10:05:45 +0700

    Create removeDuplicateCharacters.js

---
+++ removeDuplicateCharacters.js
@@ -0,0 +1,24 @@
+//chua nop
+function removeDuplicateCharacters(str){
+    var arr = [];
+    var value = [];
+    var g = 0;
+    for( var i = 0; i < str.length; i++ ){
+        arr[i] = str.slice(i,i+1);
+    }
+    for( var j = 0; j < arr.length; j++ ){
+      var number = 0;
+      for( var k = 0; k < arr.length; k++ ){
+        if( arr[j] === arr[k] ){
+          number++;
+        }
+      }
+      if( number === 1 ){
+        //for( var g = 0; g < arr.length; g++ ){
+          value[g] = arr[j];
+          g++;
+        //}
+      }
+    }
+    return value.join('');
+}
